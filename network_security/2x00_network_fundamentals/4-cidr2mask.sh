ee48458e5cf1d21dceed25b8bf3d0a69bbd5bac2
Wibi <Wibi@gmail.com>
2026-05-18 12:42:16 +0000

    Nw 4 updated

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mask=$((0xffffffff << (32 - $1))); for i in 24 16 8 0; do printf "%d%s" $(( (mask >> i) & 255 )) "$([ $i -eq 0 ] && echo -e '\n' || echo '.')\n"; done | paste -sd '' -
+mask=$((0xffffffff << (32 - $1))); octets=($((mask>>24 & 255)) $((mask>>16 & 255)) $((mask>>8 & 255)) $((mask & 255))); echo "${octets[*]/ /.}"
