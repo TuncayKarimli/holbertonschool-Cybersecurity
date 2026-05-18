464504f06a345a8cb609923fd738d4737a5b0e6f
Wibi <Wibi@gmail.com>
2026-05-18 13:20:04 +0000

    Nw 4 last one

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-bits=""; for i in {1..32}; do [ $i -le $1 ] && bits="${bits}1" || bits="${bits}0"; done
-echo "$((2#${bits:0:8})).$((2#${bits:8:8})).$((2#${bits:16:8})).$((2#${bits:24:8}))"
+bits=""; for i in {1..32}; do [ $i -le $1 ] && bits="${bits}1" || bits="${bits}0"; done; echo "$((2#${bits:0:8})).$((2#${bits:8:8})).$((2#${bits:16:8})).$((2#${bits:24:8}))"
