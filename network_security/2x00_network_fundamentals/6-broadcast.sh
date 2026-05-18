a79984e5766ba888db66a67ef2b252bde346f6e2
Wibi <Wibi@gmail.com>
2026-05-18 14:53:12 +0000

    Nw 6 upd

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS='.'; ip=($1); mask=($2); echo "$((ip[0] | (mask[0] ^ 255))).$((ip[1] | (mask[1] ^ 255))).$((ip[2] | (mask[2] ^ 255))).$((ip[3] | (mask[3] ^ 255)))"
+arg1=$(echo "$1" | tr -d '\r '); arg2=$(echo "$2" | tr -d '\r '); IFS='.'; ip=($arg1); mask=($arg2); echo "$((ip[0] | (mask[0] ^ 255))).$((ip[1] | (mask[1] ^ 255))).$((ip[2] | (mask[2] ^ 255))).$((ip[3] | (mask[3] ^ 255)))"
