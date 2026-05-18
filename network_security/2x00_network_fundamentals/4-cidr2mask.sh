558006e5227ad5a91875ab0a698f06252ac66438
Wibi <Wibi@gmail.com>
2026-05-18 12:46:42 +0000

    Nw 4 updated again again

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c "import sys; c=int(sys.argv[1]); m=0xFFFFFFFF^((1<<(32-c))-1); print('.'.join([str((m>>i)&0xFF) for i in [24,16,8,0]]))" $1
+m=$(( 0xFFFFFFFF ^ ((1 << (32 - $1)) - 1) )); echo "$(( (m >> 24) & 0xFF )).$(( (m >> 16) & 0xFF )).$(( (m >> 8) & 0xFF )).$(( m & 0xFF ))"
